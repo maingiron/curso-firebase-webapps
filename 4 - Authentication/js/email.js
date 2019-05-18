9ee6567c97b0edf2ec812f87302d42cd7fecc33b
Raphael Giron <maingiron@gmail.com>
2019-05-17 22:02:06 -0300

    auth

--- 4 - Authentication/js/email.js
+++ 4 - Authentication/js/email.js
@@ -2,14 +2,29 @@
  * Função para cadastro com email e senha
  */
 function createLogin() {
+    let email = document.getElementById("email").value
+    let senha = document.getElementById("senha").value
 
+    firebase.auth().createUserWithEmailAndPassword(email, senha).then(user => {
+        console.log("Usuário...", user)
+        alert("Usuário criado e logado.")
+    }).catch(err => {
+        console.log("Erro:", err)
+    })
 }
 
 /**
  * Função para login
  */
 function loginEmail() {
-
+    let email = document.getElementById("email").value
+    let senha = document.getElementById("senha").value
+    
+    firebase.auth().signInWithEmailAndPassword(email, senha).then(() => {
+        alert("Usuário logado")
+    }).catch(err => {
+        console.log("Erro:", err)
+    })
 }
 
 /**
