f93c4eb36fd81fca6425ef0d3edf57c5548f6366
Raphael Giron <maingiron@gmail.com>
2019-05-25 19:37:12 -0300

    Enviando arquivo

--- 3 - Storage/app.js
+++ 3 - Storage/app.js
@@ -4,11 +4,25 @@
 var fileInput = document.getElementById('file-input');
 var stringInput = document.getElementById('string-input');
 
+let ref = firebase.storage().ref('arquivos')
+
 /**
  * Metodo que observa mudanças no input de arquivo
  */
 fileInput.onchange = function (event) {
-    
+    let arquivo = event.target.files[0];
+
+    /**
+     * .child('nome') --> Acessa o caminho para inserir o arquivo
+     * .put(arquivo) --> Adiciona o arquivo
+     */
+    ref.child('arquivo').put(arquivo).then(snapshot => {
+        console.log('snapshot...', snapshot)
+        // getDownloadURL() --> Retorna a url aonde o arquivo foi hospedado
+        ref.child('arquivo').getDownloadURL().then(url => {
+            console.log('string para download...', url)
+        })
+    })
 }
 
 /**
